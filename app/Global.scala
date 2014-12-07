410548a3530751d1300d076bcc6e2fd76dc4df30
Rich Dougherty <rich@rd.gen.nz>
2014-12-08 11:11:11 +1300

    DI for filters

--- app/Global.scala
+++
@@ -1,9 +0,0 @@
-import play.api.libs.concurrent._
-import Execution.Implicits._
-import play.api.{Logger, Application}
-import play.filters.gzip.GzipFilter
-import scala.concurrent.duration._
-import play.api.mvc._
-import utils.PlayGitRepository
-
-object Global extends WithFilters(new GzipFilter(), new utils.ServerHeaderFilter)
