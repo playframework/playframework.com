4ca5dc8b7034f92feacb802c14fdf4351023c87a
Rich Dougherty <rich@rd.gen.nz>
2015-07-03 21:58:02 +1200

    Fix mixed content error for downloads
    Change downloads to use https instead of http. This is
    probably a better default anyway.

--- app/models/Releases.scala
+++ app/models/Releases.scala
@@ -2,7 +2,9 @@ package models
 
 import play.api.libs.json.Json
 
-case class PlayRelease(version: String, url: Option[String], date: Option[String], size: Option[String], name: Option[String])
+case class PlayRelease(version: String, private val url: Option[String], date: Option[String], size: Option[String], name: Option[String]) {
+  lazy val secureUrl: Option[String] = url.map(SecurifyUrl.securify)
+}
 
 object PlayRelease {
   implicit val releaseReads = Json.reads[PlayRelease]
@@ -14,9 +16,17 @@ object PlayReleases {
   implicit val playReleasesReads = Json.reads[PlayReleases]
 }
 
-case class ActivatorRelease(version: String, url: String, miniUrl: String, size: String, miniSize: String,
-  playVersion: String, akkaVersion: String, scalaVersion: String)
+case class ActivatorRelease(version: String, private val url: String, private val miniUrl: String, size: String, miniSize: String,
+  playVersion: String, akkaVersion: String, scalaVersion: String) {
+
+  lazy val secureUrl: String = SecurifyUrl.securify(url)
+  lazy val miniSecureUrl: String = SecurifyUrl.securify(miniUrl)
+}
 
 object ActivatorRelease {
   implicit val activatorReads = Json.reads[ActivatorRelease]
+}
+
+private[models] object SecurifyUrl {
+  def securify(url: String): String = url.replaceFirst("http", "https")
 }
