dd75b559044f49cab8b9abcf41d7b4d10737d272
Marcos Pereira <marcos.silva@gmail.com>
2017-05-24 16:27:43 -0300

    Remove unnecessary self type

--- app/controllers/Common.scala
+++ app/controllers/Common.scala
@@ -1,9 +1,8 @@
 package controllers
 
 import play.api.mvc._
+import play.api.mvc.Results._
 
 trait Common {
-  self: BaseController =>
-
   def notFound(implicit request: RequestHeader) = NotFound(views.html.notfound())
 }
