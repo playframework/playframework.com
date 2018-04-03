2688fcbc34bd465418576cd6d8d687c62f3eb0ef
Ruth Stento <ruth.stento@lightbend.com>
2018-04-03 11:52:07 -0500

    First set of changes to remove "download" terminology  (#192)
    * First set of changes to remove "download" and to simplify instructions for getting started
    
    * Making "Try Play" page more concise and linking to Tech Hub
    
    * Shortened title
    
    * Updated simplified version
    
    * A few tweaks
    
    * Routes and file names consistent with the pages
    
    * Remove password
    
    * Remove unused pages
    
    * Make urls protocol relative

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -130,7 +130,7 @@ $(function(){
     })
 
     // Scope on download page
-    $("body.download").each(function(el){
+    $("body.alternatives").each(function(el){
         // Show guides on download
         var download = $(".latest"),
             getStarted = $(".get-started"),
@@ -144,7 +144,7 @@ $(function(){
         });
 
         // Older releases
-        var versions = $("#alternatives .version");
+        var versions = $(".alternatives .version");
 
         versions.each(function(i, el) {
             var list = $(".release", el).slice(3).hide();
